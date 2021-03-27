a5830995ca27277c02b1197c517ee9247bdb4e04
shtesh <vshtefanuka@gmail.com>
2021-03-27 15:24:26 +0300

    Update changed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import UserDailyGoals from "./views/DailyGoals";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-import UpdateDailyGoal from "./views/UpdateDailyGoal";
 
 function App() {
   return (
@@ -35,64 +34,21 @@ function App() {
         <PrivateRoute exact path="/dailyGoal">
           <UserDailyGoals />
         </PrivateRoute>
+        <PrivateRoute path="/dailyGoal/:dailyGoalId/edit">
+          <h1>Update</h1>
+        </PrivateRoute>
         <PrivateRoute exact path="/dailyGoal/:dailyGoalId">
           <DailyGoalDetail />
-        </PrivateRoute>
-        <PrivateRoute path="/dailyGoal/:dailyGoalId/edit">
-          <UpdateDailyGoal />
         </PrivateRoute>
         <Route exact to="/">
           <h1>Home</h1>
         </Route>
         <Route path="*">
-          <ErrorBoundary>
-            <BuggyComponent />
-          </ErrorBoundary>
+          <h1>Not Found</h1>
         </Route>
       </Switch>
     </div>
   );
-  const delay = () => new Promise((res) => setTimeout(() => res(true), 2000));
-  const error = () => {
-    throw new Error("error");
-  };
-
-  function BuggyComponent() {
-    const [state, setState] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(false);
-    const [error, setError] = React.useState(false);
-
-    async function getData() {
-      try {
-        setIsLoading(true);
-        error();
-        setState(["ready", "steady", "go"]);
-        setIsLoading(false);
-      } catch (e) {
-        console.error(e);
-        setError(true);
-        setIsLoading(false);
-      }
-    }
-    React.useEffect(() => {
-      getData();
-    }, []);
-
-    if (error) {
-      return <h2>Error</h2>;
-    }
-    if (isLoading) {
-      return <h2>Loading...</h2>;
-    }
-    return (
-      <div>
-        {state.map((value) => (
-          <p>value</p>
-        ))}
-        <button onClick={() => setState({})}>break</button>
-      </div>
-    );
-  }
 }
 
 export default App;
