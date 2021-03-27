825f0294f1fda46d92c7f86675e4fc4805bcddae
shtesh <vshtefanuka@gmail.com>
2021-03-27 14:25:53 +0300

    Centering Login and signup

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -19,46 +19,48 @@ export default function Login() {
   return (
     <div className="control">
       <form onSubmit={handleSubmit}>
-        <div className="field">
-          <label htmlFor="email">Email</label>
-          <p className="control has-icons-left has-icons-right">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={formState.email}
-              onChange={handleChange}
-              placeholder="jack@gmail.com"
-              className="input"
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-envelope"></i>
-            </span>
-          </p>
-        </div>
-        <div className="field">
-          <label htmlFor="password">Password</label>
-          <p className="control has-icons-left">
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={formState.password}
-              onChange={handleChange}
-              placeholder="********"
-              className="input"
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-lock"></i>
-            </span>
-          </p>
-        </div>
-        <div className="field">
-          <p className="control">
-            <button type="submit" className="button is-info">
-              Log Me In
-            </button>
-          </p>
+        <div className="box mr-6 ml-6">
+          <div className="field">
+            <label htmlFor="email">Email</label>
+            <p className="control has-icons-left has-icons-right">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={formState.email}
+                onChange={handleChange}
+                placeholder="jack@gmail.com"
+                className="input"
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password</label>
+            <p className="control has-icons-left">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={formState.password}
+                onChange={handleChange}
+                placeholder="********"
+                className="input"
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <p className="control">
+              <button type="submit" className="button is-info">
+                Log Me In
+              </button>
+            </p>
+          </div>
         </div>
       </form>
     </div>
